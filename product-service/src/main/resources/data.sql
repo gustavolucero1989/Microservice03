9917226141656d39b55b2b1362a1985748e1ae21
gustavo <gustavolucero1989@gmail.com>
2020-10-12 16:02:36 -0300

    fixdata sql

--- product-service/src/main/resources/data.sql
+++ product-service/src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO tbl_categories (id, name) VALUES (2, 'books');
 INSERT INTO tbl_categories (id, name) VALUES (3, 'electronics');
 
 INSERT INTO tbl_products (id, name, description, stock,price,status, create_at,category_id)
-VALUES (1, 'adidas Cloudfoam Ultimate','Walk in the air in the black / black CLOUDFOAM ULTIMATE running shoe from ADIDAS',5,178.89,'CREATED','2018-09-05',1);
+VALUES (1, 'adidas Cloudfoam Ultimate pro 2020','Walk in the air in the black / black CLOUDFOAM ULTIMATE running shoe from ADIDAS',5,178.89,'CREATED','2018-09-05',1);
 
 INSERT INTO tbl_products (id, name, description, stock,price,status, create_at,category_id)
 VALUES (2, 'under armour Men ''s Micro G Assert – 7','under armour Men ''Lightweight mesh upper delivers complete breathability . Durable leather overlays for stability ',4,12.5,'CREATED','2018-09-05',1);
